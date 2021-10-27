185ae167f9e0403a924f8ced47040265f6c23f67
Col-R <2colewestin@gmail.com>
2021-10-27 06:16:44 -0500

    Added responses and compiled

--- AlfredQuotes.java
+++ AlfredQuotes.java
@@ -4,18 +4,11 @@ public class AlfredQuotes{
         return "Hello, lovely to see you. How are you?";
     }
     public String guestGreeting (String name, String dayPeriod){
-        //TODO
         return (String.format("Good %s, %s. Lovely to see you", name, dayPeriod));
     }
     public String dateAnnoucement(){
-        
-        public static void main(String[] args){
-            AlfredQuotes id = new AlfredQuotes();
-            String dateAnnoucement = id.getCurrentDate();
-            System.out.println(dateAnnoucement);
-        }
-        Date date = new Date();
-        return "Current date is " + date;
+
+        return "Current date is ";
     }
     public String respondBeforeAlexis(String conversation) {
         return "placeholder for snarky response return string";
